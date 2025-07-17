b07182b7777115e4b2cf0c3a5aea46653216ec62
LuigiAnaclet <124635849+LuigiAnaclet@users.noreply.github.com>
2025-07-17 10:21:28 -0400

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,18 +1,18 @@
 const { Pool } = require('pg');
 
-// Configuration de la connexion PostgreSQL
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false // nécessaire pour Render
+  }
 });
 
-
-// Vérification de la connexion
 pool.connect()
-  .then(() => console.log("Connecté à PostgreSQL"))
-  .catch(err => console.error("Erreur de connexion à PostgreSQL", err));
+  .then(() => console.log("✅ Connecté à PostgreSQL avec SSL"))
+  .catch(err => console.error("❌ Erreur de connexion à PostgreSQL", err));
 
-  module.exports = pool;
+module.exports = pool;
